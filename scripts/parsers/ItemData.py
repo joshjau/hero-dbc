b2d19194ee44cdbba7facba93290d513597c92bb
Joshua <37181202+joshjau@users.noreply.github.com>
2025-02-01 11:13:22 +0800

    Update ItemData.py

--- scripts/parsers/ItemData.py
+++ scripts/parsers/ItemData.py
@@ -10,234 +10,182 @@ import sys
 import os
 import csv
 import json
+from typing import Dict, List, Any
+from dataclasses import dataclass
+from enum import Enum, auto
 
 generatedDir = os.path.join('scripts', 'DBC', 'generated')
 parsedDir = os.path.join('scripts', 'DBC', 'parsed')
-ItemDataList = ""
-LegendaryDataList = ""
-
-classTable = {}
-encounterTable = {}
-
-checkLeg = False
 
 os.chdir(os.path.join(os.path.dirname(sys.path[0]), '..', '..', 'hero-dbc'))
 
-def computeItemType(x):
-    return {
-        1: 'head',
-        2: 'neck',
-        3: 'shoulders',
-        5: 'chest',
-        6: 'waist',
-        7: 'legs',
-        8: 'feet',
-        9: 'wrists',
-        10: 'hands',
-        11: 'finger',
-        12: 'trinket',
-        13: 'weapon',
-        14: 'shield',
-        15: 'ranged',
-        16: 'back',
-        17: '2hweapon',
-        20: 'chest'
-    }.get(x, '')
+class ItemType(Enum):
+    HEAD = 1
+    NECK = 2
+    SHOULDERS = 3
+    CHEST = 5
+    WAIST = 6
+    LEGS = 7
+    FEET = 8
+    WRISTS = 9
+    HANDS = 10
+    FINGER = 11
+    TRINKET = 12
+    WEAPON = 13
+    SHIELD = 14
+    RANGED = 15
+    BACK = 16
+    TWO_HAND_WEAPON = 17
+    CHEST_ALT = 20
 
-def computeItemMaterial(x):
-    return {
-        6: 'plate',
-        5: 'mail',
-        8: 'leather',
-        7: 'cloth'
-    }.get(x, '')
+class ArmorType(Enum):
+    PLATE = 6
+    MAIL = 5
+    LEATHER = 8
+    CLOTH = 7
 
-def createSpecTable():
-    global classTable
+class StatType(Enum):
+    AGI = 3
+    STR = 4
+    INT = 5
+    STAM = 7
+    CRIT = 32
+    HASTE = 36
+    VERS = 40
+    MASTERY = 49
+    BONUS_ARMOR = 50
+    LEECH = 62
+    AVOIDANCE = 63
+    AGI_STR_INT = 71
+    AGI_STR = 72
+    AGI_INT = 73
+    STR_INT = 74
 
-    # Death Knight
-    classTable['death_knight'] = {}
-    classTable['death_knight'][0] = 'blood'
-    classTable['death_knight'][1] = 'frost'
-    classTable['death_knight'][2] = 'unholy'
-    # Demon Hunter
-    classTable['demon_hunter'] = {}
-    classTable['demon_hunter'][0] = 'havoc'
-    classTable['demon_hunter'][1] = 'vengeance'
-    # Druid                    
-    classTable['druid'] = {}
-    classTable['druid'][0] = 'balance'
-    classTable['druid'][1] = 'feral'
-    classTable['druid'][2] = 'guardian'
-    classTable['druid'][3] = 'restoration'
-    # Hunter 
-    classTable['hunter'] = {}
-    classTable['hunter'][0] = 'beast_mastery'
-    classTable['hunter'][1] = 'marksmanship'
-    classTable['hunter'][2] = 'survival'
-    # Mage 
-    classTable['mage'] = {}
-    classTable['mage'][0] = 'arcane'
-    classTable['mage'][1] = 'fire'
-    classTable['mage'][2] = 'frost'
-    # Monk 
-    classTable['monk'] = {}
-    classTable['monk'][0] = 'brewmaster'
-    classTable['monk'][1] = 'windwalker'
-    classTable['monk'][2] = 'mistweaver'
-    # Paladin 
-    classTable['paladin'] = {}
-    classTable['paladin'][0] = 'holy'
-    classTable['paladin'][1] = 'protection'
-    classTable['paladin'][2] = 'retribution'
-    # Priest 
-    classTable['priest'] = {}
-    classTable['priest'][0] = 'discipline'
-    classTable['priest'][1] = 'holy'
-    classTable['priest'][2] = 'shadow'
-    # Rogue 
-    classTable['rogue'] = {}
-    classTable['rogue'][0] = 'assassination'
-    classTable['rogue'][1] = 'outlaw'
-    classTable['rogue'][2] = 'subtlety'
-    # Shaman 
-    classTable['shaman'] = {}
-    classTable['shaman'][0] = 'elemental'
-    classTable['shaman'][1] = 'enhancement'
-    classTable['shaman'][2] = 'restoration'
-    # Warlock 
-    classTable['warlock'] = {}
-    classTable['warlock'][0] = 'affliction'
-    classTable['warlock'][1] = 'demonology'
-    classTable['warlock'][2] = 'destruction'
-    # Warrior 
-    classTable['warrior'] = {}
-    classTable['warrior'][0] = 'arms'
-    classTable['warrior'][1] = 'fury'
-    classTable['warrior'][2] = 'protection'
+@dataclass
+class ItemData:
+    """Store item data with type information."""
+    id: int
+    name: str
+    ilevel: int
+    type: str
+    material: str
+    stats: List[str]
+    gems: int
+    source: str
 
-def computeSet(set, ilvl):
-    if set == 0:
-        return ""
-    else:
-        print("unknown set:"+str(ilvl))
-        return ""
+def get_item_type(inv_type: int) -> str:
+    """Get item type string from inventory type ID."""
+    try:
+        return ItemType(inv_type).name.lower().replace('_', '')
+    except ValueError:
+        return ''
 
-def computeSource(set, quality, id, type, ilvl):
-    if ilvl == 158 :
-        if id not in encounterTable:
-            return "pvp"
-        else:
-            return "dungeon"
-    if ilvl == 200 :
-        if id not in encounterTable:
-            return "other" #todo : craft / pvp / boe
-        else:
-            return "castle_nathria"
-    if ilvl == 226 or ilvl == 233 :
+def get_armor_type(material: int) -> str:
+    """Get armor material type string from material ID."""
+    try:
+        return ArmorType(material).name.lower()
+    except ValueError:
+        return ''
+
+def get_stat_type(stat_type: int) -> str:
+    """Get stat type string from stat type ID."""
+    try:
+        return StatType(stat_type).name.lower()
+    except ValueError:
+        return ''
+
+def compute_gem_slots(row: Dict[str, str]) -> int:
+    """Calculate number of gem slots from item data."""
+    gem_count = 0
+    for i in range(1, 4):
+        if int(row[f'socket_color_{i}']) != 0:
+            gem_count += 1
+    return gem_count
+
+def get_item_source(item_id: int, ilvl: int, encounters: Dict[int, int]) -> str:
+    """Determine item source based on item level and encounter data."""
+    if ilvl == 158:
+        return "dungeon" if item_id in encounters else "pvp"
+    elif ilvl == 200:
+        return "castle_nathria" if item_id in encounters else "other"
+    elif ilvl in (226, 233):
         return "sanctum_of_domination"
-
-    print("unknown source:"+str(id))
     return ""
 
-def computeItemStat(mask):
-    return {
-        3: 'agi',
-        4: 'str',
-        5: 'int',
-        7: 'stam',
-        32: 'crit',
-        36: 'haste',
-        40: 'vers',
-        49: 'mastery',
-        50: 'bonus_armor',
-        62: 'leech',
-        63: 'avoidance',
-        71: 'agi/str/int',
-        72: 'agi/str',
-        73: 'agi/int',
-        74: 'str/int',
-    }.get(mask, '')
-
-
-def getItemStats(row):
+def get_item_stats(row: Dict[str, str]) -> List[str]:
+    """Get list of item stats from item data."""
     stats = []
-    computed = computeItemStat(int(row['stat_type_1']))
-    if computed != '':
-        stats.append(computed)
-    computed = computeItemStat(int(row['stat_type_2']))
-    if computed != '':
-        stats.append(computed)
-    computed = computeItemStat(int(row['stat_type_3']))
-    if computed != '':
-        stats.append(computed)
-    computed = computeItemStat(int(row['stat_type_4']))
-    if computed != '':
-        stats.append(computed)
-    computed = computeItemStat(int(row['stat_type_5']))
-    if computed != '':
-        stats.append(computed)
+    for i in range(1, 6):
+        stat_type = int(row[f'stat_type_{i}'])
+        stat = get_stat_type(stat_type)
+        if stat:
+            stats.append(stat)
     return stats
 
-def computeGemNumber(row):
-    gemnb = 0
-    if not int(row['socket_color_1']) == 0:
-        gemnb = gemnb + 1
-        if not int(row['socket_color_2']) == 0:
-            gemnb = gemnb + 1
-            if not int(row['socket_color_3']) == 0:
-                gemnb = gemnb + 1
-    return gemnb
-
-
-def PrepareRow(row, rowClass="", rowSpec=""):
-    preparedRow = {}
-    preparedRow["id"] = int(row['id'])
-    preparedRow["name"] = row['name']
-    preparedRow["ilevel"] = int(row['ilevel'])
-    preparedRow["type"] = computeItemType(int(row['inv_type']))
-    preparedRow["material"] = computeItemMaterial(int(row['material']))
-    preparedRow["stats"] = getItemStats(row)
-
-    # set = computeSet(int(row['item_set']), int(row['ilevel']))
-    # preparedRow["set"] = computeSet(int(row['item_set']), int(row['ilevel']))
-    preparedRow["gems"] = computeGemNumber(row)
-    preparedRow["source"] = computeSource(set, int(row['quality']), int(row['id']), preparedRow["type"], int(row['ilevel']))
-
-    return preparedRow
-
-
-# Program Start
-createSpecTable()
-
-with open(os.path.join(generatedDir, f'JournalEncounterItem.csv')) as csvfile:
-    reader = csv.DictReader(csvfile, escapechar='\\')
-    for row in reader:
-        encounterTable[int(row['id_item'])] = int(row['id_encounter'])
-
-with open(os.path.join(generatedDir, 'ItemSparse.csv')) as csvfile:
-    reader = csv.DictReader(csvfile, escapechar='\\')
-    reader = sorted(reader, key=lambda d: int(d['id']))
-    ValidItemsRows = {}
-
-    # Read rows and order them with each inventory type, class and material
-    for row in reader:
-        # ilvl : 158 = dungeon, 200 = castle nathria, 226/233 = Sanctum of domination
-        if int(row['ilevel']) == 158 or int(row['ilevel']) == 200 or int(row['ilevel']) == 226 or int(row['ilevel']) == 233:
-            itemType = computeItemType(int(row['inv_type']))
-            itemMaterial = computeItemMaterial(int(row['material']))
-            if not itemType == "trinket" and not itemType == "neck" and not itemType == "finger" and not itemType == "back":  # handle no material separatly
-                if itemType not in ValidItemsRows:
-                    ValidItemsRows[itemType] = {}
-                if itemMaterial not in ValidItemsRows[itemType]:
-                    ValidItemsRows[itemType][itemMaterial] = []
-                ValidItemsRows[itemType][itemMaterial].append(PrepareRow(row))
+def process_item_data(encounters: Dict[int, int]) -> Dict[str, Any]:
+    """Process item data from ItemSparse.csv."""
+    valid_items: Dict[str, Any] = {}
+    
+    with open(os.path.join(generatedDir, 'ItemSparse.csv')) as csvfile:
+        reader = csv.DictReader(csvfile, escapechar='\\')
+        for row in reader:
+            ilvl = int(row['ilevel'])
+            if ilvl not in (158, 200, 226, 233):
+                continue
+                
+            item_type = get_item_type(int(row['inv_type']))
+            if not item_type:
+                continue
+                
+            item_data = ItemData(
+                id=int(row['id']),
+                name=row['name'],
+                ilevel=ilvl,
+                type=item_type,
+                material=get_armor_type(int(row['material'])),
+                stats=get_item_stats(row),
+                gems=compute_gem_slots(row),
+                source=get_item_source(int(row['id']), ilvl, encounters)
+            )
+            
+            # Organize items by type and material
+            if item_type not in ('trinket', 'neck', 'finger', 'back'):
+                if item_type not in valid_items:
+                    valid_items[item_type] = {}
+                if item_data.material not in valid_items[item_type]:
+                    valid_items[item_type][item_data.material] = []
+                valid_items[item_type][item_data.material].append(item_data.__dict__)
             else:
-                if itemType not in ValidItemsRows:
-                    ValidItemsRows[itemType] = []
-                ValidItemsRows[itemType].append(PrepareRow(row))
+                if item_type not in valid_items:
+                    valid_items[item_type] = []
+                valid_items[item_type].append(item_data.__dict__)
+    
+    return valid_items
 
+def load_encounters() -> Dict[int, int]:
+    """Load encounter data from JournalEncounterItem.csv."""
+    encounters = {}
+    with open(os.path.join(generatedDir, 'JournalEncounterItem.csv')) as csvfile:
+        reader = csv.DictReader(csvfile, escapechar='\\')
+        for row in reader:
+            encounters[int(row['id_item'])] = int(row['id_encounter'])
+    return encounters
 
-    # Prints everything to the files
-    with open(os.path.join(parsedDir, 'ItemData.json'), 'w', encoding='utf-8') as file:
-        json.dump(ValidItemsRows, file, indent=4)
+def write_json_output(item_data: Dict[str, Any]):
+    """Write the optimized JSON output file."""
+    output_path = os.path.join(parsedDir, 'ItemData.json')
+    with open(output_path, 'w', encoding='utf-8') as file:
+        json.dump(item_data, file, indent=4)
+
+def main():
+    # Load encounter data
+    encounters = load_encounters()
+    
+    # Process item data
+    item_data = process_item_data(encounters)
+    
+    # Generate output
+    write_json_output(item_data)
+
+if __name__ == '__main__':
+    main()
